634025145bbb758f89e16924dbcb88af7486805b
Clarence Ho <clarenceho@gmail.com>
2015-11-28 00:04:50 -0800

    first commit

---
+++ src/net/clarenceho/ssl/PrefSSLSocketFactory.java
@@ -0,0 +1,159 @@
+package net.clarenceho.ssl;
+
+import java.util.List;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+
+/**
+ * Custom SSL Socket Factory to specify specific cipher suites
+ *
+ * @author clarenceho at gmail dot com
+ *
+ */
+public class PrefSSLSocketFactory extends SSLSocketFactory {
+
+    private SSLSocketFactory factory;
+    private String[] ciphers;
+    
+    /**
+     * Creates instance of the factory by specifying the protocol
+     * 
+     * @param protocol SSLContext algorithm to use. e.g. SSL, SSLv2, SSLv3, TLS, TLSv1, TLSv1.1, TLSv1.2
+     * @throws KeyManagementException
+     * @throws NoSuchAlgorithmException
+     */
+    public PrefSSLSocketFactory(String protocol) throws KeyManagementException, NoSuchAlgorithmException  {
+        
+        SSLContext context = SSLContext.getInstance(protocol);
+        // use default security providers and random parameter
+        context.init(null, null, null);
+        this.factory = (context.getSocketFactory());
+
+        setCipher(new ArrayList<String>(
+                Arrays.asList(this.factory.getDefaultCipherSuites())));
+    }
+
+    /**
+     * Creates instance of the factory by providing a delegate factory
+     * 
+     * @param factory The delegate factory
+     */
+    public PrefSSLSocketFactory(SSLSocketFactory factory) {
+        // delegate SSL functions to helper
+        this.factory = factory;
+
+        setCipher(new ArrayList<String>(
+                Arrays.asList(this.factory.getDefaultCipherSuites())));
+    }
+
+    
+    /**
+     * Uses ECDHE cipher suites only
+     */
+    public void useECDHEOnly() {
+        String[] currentCiphers = this.factory.getDefaultCipherSuites();
+        ArrayList<String> newCiphers = new ArrayList<String>();
+        for (String cipher : currentCiphers) {
+            if (cipher.contains("_ECDHE_")) {
+                newCiphers.add(cipher);
+            }
+        }
+        setCipher(newCiphers);        
+    }
+
+    /**
+     * Doesn't use forward secrecy ciphers.  Note that this is considered to be UNSAFE!!!
+     */
+    public void useNoForwardSecrecy() {
+        String[] currentCiphers = this.factory.getDefaultCipherSuites();
+        ArrayList<String> newCiphers = new ArrayList<String>();
+        for (String cipher : currentCiphers) {
+            if (!(cipher.contains("_ECDHE_") || cipher.contains("_DHE_"))) {
+                newCiphers.add(cipher);
+            }
+        }
+        setCipher(newCiphers);        
+        
+    }
+
+    /**
+     * Uses forward secrecy ciphers.  Note that this is considered to be UNSAFE!!!
+     */
+    public void useForwardSecrecy() {
+        String[] currentCiphers = this.factory.getDefaultCipherSuites();
+        ArrayList<String> newCiphers = new ArrayList<String>();
+        for (String cipher : currentCiphers) {
+            if ((cipher.contains("_ECDHE_") || cipher.contains("_DHE_"))) {
+                newCiphers.add(cipher);
+            }
+        }
+        setCipher(newCiphers);        
+        
+    }
+
+    /**
+     * Specifies the cipher suites to use.  Otherwise, will use the factory default
+     * 
+     * @param ciphers
+     */
+    public void setCipher(List<String>ciphers) {
+        this.ciphers = ciphers.toArray(new String[ciphers.size()]);
+    }
+
+    
+    @Override
+    public Socket createSocket(Socket arg0, String arg1, int arg2, boolean arg3) throws IOException {
+        SSLSocket socket = (SSLSocket)this.factory.createSocket(arg0, arg1, arg2, arg3);
+        socket.setEnabledCipherSuites(this.ciphers);
+        return socket;
+    }
+
+    @Override
+    public String[] getDefaultCipherSuites() {
+        return this.ciphers;
+    }
+
+    @Override
+    public String[] getSupportedCipherSuites() {
+        return this.ciphers;
+    }
+
+    @Override
+    public Socket createSocket(String arg0, int arg1) throws IOException, UnknownHostException {
+        SSLSocket socket = (SSLSocket)this.factory.createSocket(arg0, arg1);
+        socket.setEnabledCipherSuites(this.ciphers);        
+        return socket;
+    }
+
+    @Override
+    public Socket createSocket(InetAddress arg0, int arg1) throws IOException {
+        SSLSocket socket = (SSLSocket)this.factory.createSocket(arg0, arg1);
+        socket.setEnabledCipherSuites(this.ciphers);        
+        return socket;
+    }
+
+    @Override
+    public Socket createSocket(String arg0, int arg1, InetAddress arg2, int arg3)
+            throws IOException, UnknownHostException {
+        SSLSocket socket = (SSLSocket)this.factory.createSocket(arg0, arg1, arg2, arg3);
+        socket.setEnabledCipherSuites(this.ciphers);        
+        return socket;
+    }
+
+    @Override
+    public Socket createSocket(InetAddress arg0, int arg1, InetAddress arg2, int arg3) throws IOException {
+        SSLSocket socket = (SSLSocket)this.factory.createSocket(arg0, arg1, arg2, arg3);
+        socket.setEnabledCipherSuites(this.ciphers);        
+        return socket;
+    }
+}
---
+++ src/net/clarenceho/ssl/test/TestPrefSSLSocketFactory.java
@@ -0,0 +1,171 @@
+package net.clarenceho.ssl.test;
+
+import org.junit.*;
+
+import net.clarenceho.ssl.PrefSSLSocketFactory;
+
+import static org.junit.Assert.*;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestPrefSSLSocketFactory {
+
+    @BeforeClass
+    public static void oneTimeSetup() {
+        try {
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, null, null);
+            SSLSocketFactory factory = context.getSocketFactory();
+            System.out.println("Default TLS cipher suites:");
+            printDefaultCiphers(factory);
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+
+    }
+
+    @AfterClass
+    public static void oneTimeCleanup() {
+
+    }
+
+    @Test
+    public void testCustomDelegate() throws Exception {
+        try {
+            SSLContext context = SSLContext.getInstance("TLS");
+            // use default security providers and random parameter
+            context.init(null, null, null);
+            SSLSocketFactory prefFactory = new PrefSSLSocketFactory(context.getSocketFactory());
+            testConnection(prefFactory);
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            throw(e);
+        }
+    }
+    
+    @Test
+    public void testCreateTLS12() throws Exception {
+        try {
+            SSLSocketFactory prefFactory = new PrefSSLSocketFactory("TLSv1.2");
+            testConnection(prefFactory);
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            throw(e);
+        }
+    }
+    
+    @Test
+    public void testCreateSSL3() throws Exception {
+        try {
+            SSLSocketFactory prefFactory = new PrefSSLSocketFactory("SSLv3");
+            testConnection(prefFactory);
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            throw(e);
+        }
+    }
+    
+    @Test
+    public void testECDHEOnly() throws Exception {
+        try {
+            PrefSSLSocketFactory prefFactory = new PrefSSLSocketFactory("TLS");
+            prefFactory.useECDHEOnly();
+            String cipher = testConnection(prefFactory);
+            assertTrue(cipher.contains("_ECDHE_"));
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            throw(e);
+        }
+    }
+
+    @Test
+    public void testNoForwardSecrecy() throws Exception {
+        try {
+            PrefSSLSocketFactory prefFactory = new PrefSSLSocketFactory("TLS");
+            prefFactory.useNoForwardSecrecy();
+            String cipher = testConnection(prefFactory);
+            assertTrue(!cipher.contains("_ECDHE_"));
+            assertTrue(!cipher.contains("_DHE_"));
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            throw(e);
+        }
+    }
+
+    @Test
+    public void testForwardSecrecy() throws Exception {
+        try {
+            PrefSSLSocketFactory prefFactory = new PrefSSLSocketFactory("TLS");
+            prefFactory.useForwardSecrecy();
+            String cipher = testConnection(prefFactory);
+            assertTrue(cipher.contains("_ECDHE_") || cipher.contains("_DHE_"));
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            throw(e);
+        }
+    }
+    
+    @Test
+    public void testSpecificCipher01() throws Exception {
+        try {
+            PrefSSLSocketFactory prefFactory = new PrefSSLSocketFactory("TLS");
+            ArrayList<String> ciphers = new ArrayList<String>();
+            ciphers.add("TLS_RSA_WITH_AES_128_CBC_SHA256");
+            prefFactory.setCipher(ciphers);
+            String cipher = testConnection(prefFactory);
+            assertEquals(cipher, "TLS_RSA_WITH_AES_128_CBC_SHA256");
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            throw(e);
+        }
+    }
+    
+    @Test
+    public void testSpecificCipher02() throws Exception {
+        try {
+            PrefSSLSocketFactory prefFactory = new PrefSSLSocketFactory("TLS");
+            ArrayList<String> ciphers = new ArrayList<String>();
+            ciphers.add("TLS_RSA_WITH_AES_128_CBC_SHA");
+            ciphers.add("TLS_ECDH_RSA_WITH_AES_128_CBC_SHA256");
+            ciphers.add("TLS_ECDHE_ECDSA_WITH_RC4_128_SHA");
+            prefFactory.setCipher(ciphers);
+            String cipher = testConnection(prefFactory);
+            assertTrue(
+                    cipher.contains("TLS_RSA_WITH_AES_128_CBC_SHA") ||
+                    cipher.contains("TLS_ECDH_RSA_WITH_AES_128_CBC_SHA256") ||
+                    cipher.contains("TLS_ECDHE_ECDSA_WITH_RC4_128_SHA")
+                    );
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            throw(e);
+        }
+    }
+    
+
+    private String testConnection(SSLSocketFactory factory) throws Exception {
+        String url = "https://www.google.com/";
+        HttpsURLConnection connection = (HttpsURLConnection) (new URL(url)).openConnection();
+        connection.setSSLSocketFactory(factory);
+        connection.connect();
+        String cipherSuite = connection.getCipherSuite();
+        System.out.println("SSL connection established with cipher:" + cipherSuite);
+        assertEquals(connection.getResponseCode(), 200);
+        connection.disconnect();
+        return cipherSuite;
+    }
+
+    
+    private static void printDefaultCiphers(SSLSocketFactory factory) {
+        for (String cipher : factory.getDefaultCipherSuites()) {
+            System.out.println(cipher);
+        }
+    }
+}
